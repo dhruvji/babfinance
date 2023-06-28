6a69e33ce8c135e87a27205f0c078053bf1f7334
Dhruv Gautam <dhruvgautam@berkeley.edu>
2023-06-28 16:36:38 -0700

    main func

--- index.js
+++ index.js
@@ -12,6 +12,64 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
+const userEmail = 'dhruvgautam@berkeley.edu'; // Specify the user's email here
+main(userEmail).catch(console.error);
+
+async function main(email) {
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: 'v4', auth: client });
+
+  const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1n5teNWMzyZF8mfHRhg7YR9d00Gswrp_G8AjDALix3-8/edit?usp=sharing';
+  const spreadsheetId = getSpreadsheetId(spreadsheetUrl);
+  const range = 'Sheet1!A1:G'; // Adjust the range to cover all the columns
+
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+
+    const values = response.data.values;
+    if (values.length) {
+      console.log('Data for', email + ':');
+      values.forEach((row) => {
+        const [timestamp, userEmail, name, id, category, date, amount, userEmail2, description, link, reimbursement] = row;
+        if (userEmail === email || userEmail2 === email) {
+          console.log('Submitted Timestamp:', timestamp);
+          console.log('Email:', userEmail);
+          console.log('Name:', name);
+          console.log('Id:', id);
+          console.log('Account Billed:', category);
+          console.log('Date:', date);
+          console.log('Amount:', amount);
+          console.log('Paypal Email:', userEmail2);
+          console.log('Description:', description);
+          console.log('Receipt Link:', link);
+          console.log('Reimbursement:', reimbursement);
+          console.log('------------------');
+        }
+      });
+    } else {
+      console.log('No data found.');
+    }
+  } catch (err) {
+    console.error('The API returned an error:', err.message);
+  }
+}
+
+function getSpreadsheetId(url) {
+  const match = url.match(/\/spreadsheets\/d\/([a-zA-Z0-9-_]+)/);
+  return match && match[1] ? match[1] : null;
+}
+
+
+
+function getSpreadsheetId(url) {
+    const match = url.match(/\/spreadsheets\/d\/([a-zA-Z0-9-_]+)/);
+    return match && match[1] ? match[1] : null;
+  }
+
+
 /**
  * Reads previously authorized credentials from the save file.
  *
